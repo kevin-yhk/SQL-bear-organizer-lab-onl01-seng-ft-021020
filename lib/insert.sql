acf582ca88d11fb9ba235127e5800e035601691b
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-09 23:56:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,12 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "male", "black", "angry", True)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+"Rowdy", 2, "female", "brown", "calm", True)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+"Tabitha", 3, "male", "red", "aloof", False)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+"Sergant Brown", 4, "female", "yellow", "timid", False)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+"Melissa", 5, "male", "purple", "")
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
