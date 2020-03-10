9aa64b2c48ac57267f63c78e186fa25144353748
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-10 00:02:51 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,15 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "male", "black", "angry", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "male", "black", "angry", True);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
-"Rowdy", 2, "female", "brown", "calm", 1)
+"Rowdy", 2, "female", "brown", "calm", True);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
-"Tabitha", 3, "male", "red", "aloof", 0)
+"Tabitha", 3, "male", "red", "aloof", False);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
-"Sergant Brown", 4, "female", "yellow", "timid", 0)
+"Sergant Brown", 4, "female", "yellow", "timid", False);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
-"Melissa", 5, "male", "purple", "adamant", 1)
+"Melissa", 5, "male", "purple", "adamant", True);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
-"Grinch", 6, "female", "green", "hardy", 1)
+"Grinch", 6, "female", "green", "hardy", True);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
-"Wendy", 7, "male", "gold", "modest", 0)
+"Wendy", 7, "male", "gold", "modest", False);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
-NULL, 8, "female", "platinum", "brave", 0)
+NULL, 8, "female", "platinum", "brave", False);
